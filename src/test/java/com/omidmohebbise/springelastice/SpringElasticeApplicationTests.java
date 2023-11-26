346c9936357ca55149c7e0b64e4effab7acc9f17
omidmohebbi <omid.mohebbi@codenomads.nl>
2023-11-26 11:55:58 +0100

    initial prj

---
+++ src/test/java/com/omidmohebbise/springelastice/SpringElasticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omidmohebbise.springelastice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringElasticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
